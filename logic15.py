9f88875f942616aa6ab1bb600ecef55130a9bfbd
azimabdurasulov <112892181+azimabdurasulov@users.noreply.github.com>
2022-10-05 09:51:31 +0500

    15-topshiriq

--- logic15.py
+++ logic15.py
@@ -6,4 +6,9 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    x1 = a // 100
+    x2 = a % 100 // 10
+    x3 = a % 10
+    return a // 100 > 0 and (x1 + x2 + x3) % 2 == 1
+
+print(main(436))
