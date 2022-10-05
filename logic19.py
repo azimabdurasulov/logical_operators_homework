93b3742092f426ada5ae3377974902c75b37e59a
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-05 13:18:21 +0500

    logic19.py

--- logic19.py
+++ logic19.py
@@ -11,9 +11,14 @@ def main(x):
     Returns:
         bool: answer
     """
-    x1 = x // 100
-    x2 = x % 100 // 10
+    x1 = x % 10
+    x //= 10
+
+    x2 = x % 10
+    x //= 10
+
     x3 = x % 10
-    return x3 * 100 + x2 * 10 + x1 == x
+    x //= 10
+    return x3 * 100 + x2 * 10 + x1 == x1 * 100 + x2 * 10 + x3
 
 print(main(454))
