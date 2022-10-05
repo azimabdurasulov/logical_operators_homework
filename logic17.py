09148fad81b0e1c5918b1e7833ea4c615ec31765
azimabdurasulov <azimjon20050604@gmail.com>
2022-10-05 12:56:49 +0500

    17-topshiriq

--- logic17.py
+++ logic17.py
@@ -6,4 +6,21 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    b = a
+    x1 = b % 10
+    b //= 10
+
+    x2 = b % 10
+    b //= 10
+
+    x3 = b % 10
+    b //= 10
+
+    x4 = b % 10
+    b //= 10
+
+    x5 = b % 10
+    b //= 10
+    return x1 < x2 < x3 < x4 < x5 
+
+print(main(98765))
